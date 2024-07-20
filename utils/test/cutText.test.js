1d598eb2697f4ec24ea9b2c1fbf7834432d331b0
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-20 22:07:43 +0200

    Add test if content arg is not a string

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -20,3 +20,11 @@ it('should return an error if "content" arg is not a string', () => {
 it('should return an error if "content" arg length is 0', () => {
   expect(cutText('', 20)).to.equal('Error');
 });
+
+it('should return an error if "maxLength" arg is not a number', () => {
+  expect(cutText('Lorem Ipsum', undefined)).to.equal('Error');
+  expect(cutText('Lorem Ipsum', 'abc')).to.equal('Error');
+  expect(cutText('Lorem Ipsum', {})).to.equal('Error');
+  expect(cutText('Lorem Ipsum', [])).to.equal('Error');
+  expect(cutText('Lorem Ipsum', function() {})).to.equal('Error');
+});
