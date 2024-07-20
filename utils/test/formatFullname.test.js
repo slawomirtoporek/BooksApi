abfd5e08672a6afd6d4b4dc9a664873b3744fc81
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-20 22:54:17 +0200

    Add test if no arguments are given

--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -3,12 +3,16 @@ const expect = require('chai').expect;
 
 describe('FormatFullname', () => {
 
-    it('should return an error if "fullName" arg is not a string', () => {
-        expect(formatFullname(undefined)).to.equal('Error');
-        expect(formatFullname(12)).to.equal('Error');
-        expect(formatFullname({})).to.equal('Error');
-        expect(formatFullname([])).to.equal('Error');
-        expect(formatFullname(function() {})).to.equal('Error');
-      });
-      
+  it('should return an error if "fullName" arg is not a string', () => {
+    expect(formatFullname(undefined)).to.equal('Error');
+    expect(formatFullname(12)).to.equal('Error');
+    expect(formatFullname({})).to.equal('Error');
+    expect(formatFullname([])).to.equal('Error');
+    expect(formatFullname(function() {})).to.equal('Error');
+  });
+
+  it('should return "Error" if no arguments are given', () => {
+    expect(formatFullname()).to.equal('Error');
+  });
+
 });
